aa964c0a65f31f37473789322a766d594327c42f
amir <amir@mindtheapps.com>
2018-04-09 23:51:28 +0300

    added missing files

---
+++ app/src/main/java/com/amiru/shenkar2018/MyListItem.java
@@ -0,0 +1,19 @@
+package com.amiru.shenkar2018;
+
+/**
+ * Created by amir on 4/7/18.
+ */
+
+class MyListItem {
+    private final String title;
+
+    public MyListItem(String title) {
+
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
